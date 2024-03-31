6e19e348b12d42012206de31e2514fcadbae530d
simonemoretti94 <simone.moretti.944@gmail.com>
2024-03-31 22:14:36 +0200

    50:04

--- src/components/Posts/PostsBody.vue
+++ src/components/Posts/PostsBody.vue
@@ -21,61 +21,61 @@ export default {
                 </div>
                 <div class="table-row">
                     <div class="status"><i class="fa fa-fire"></i></div>
-                    <div class="subjects"><a href="#">Is learning Python worth it in 2024?</a><br><span>Started by <b><a
-                                    href="#">User</a></b>.</span></div>
+                    <div class="subjects"><a href="#">Is learning HTML5 worth it in 2024?</a><br><span>Started by <b><a
+                                    href="#">Simone</a></b>.</span></div>
                     <div class="replies">
                         2 replies <br> 125 views
                     </div>
                     <div class="last-reply">
-                        March 2024 <br> By <b><a href="#">User</a></b>
+                        March 2024 <br> By <b><a href="#">Simone</a></b>
                     </div>
                 </div>
 
                 <div class="table-row">
-                    <div class="status"><i class="fa fa-fire"></i></div>
-                    <div class="subjects"><a href="#">Is learning Python worth it in 2024?</a><br><span>Started by <b><a
-                                    href="#">User</a></b>.</span></div>
+                    <div class="status"><i class="fa fa-cat"></i></div>
+                    <div class="subjects"><a href="#">Is learning CSS worth it in 2024?</a><br><span>Started by <b><a
+                                    href="#">Luca</a></b>.</span></div>
                     <div class="replies">
                         2 replies <br> 125 views
                     </div>
                     <div class="last-reply">
-                        March 2024 <br> By <b><a href="#">User</a></b>
+                        March 2024 <br> By <b><a href="#">Luca</a></b>
                     </div>
                 </div>
 
                 <div class="table-row">
-                    <div class="status"><i class="fa fa-fire"></i></div>
-                    <div class="subjects"><a href="#">Is learning Python worth it in 2024?</a><br><span>Started by <b><a
-                                    href="#">User</a></b>.</span></div>
+                    <div class="status"><i class="fa fa-rocket"></i></div>
+                    <div class="subjects"><a href="#">Is learning JAVASCRIPT worth it in 2024?</a><br><span>Started by
+                            <b><a href="#">Fabio</a></b>.</span></div>
                     <div class="replies">
                         2 replies <br> 125 views
                     </div>
                     <div class="last-reply">
-                        March 2024 <br> By <b><a href="#">User</a></b>
+                        March 2024 <br> By <b><a href="#">Fabio</a></b>
                     </div>
                 </div>
 
                 <div class="table-row">
-                    <div class="status"><i class="fa fa-fire"></i></div>
-                    <div class="subjects"><a href="#">Is learning Python worth it in 2024?</a><br><span>Started by <b><a
-                                    href="#">User</a></b>.</span></div>
+                    <div class="status"><i class="fa fa-book"></i></div>
+                    <div class="subjects"><a href="#">Is learning PHP worth it in 2024?</a><br><span>Started by <b><a
+                                    href="#">Sara</a></b>.</span></div>
                     <div class="replies">
                         2 replies <br> 125 views
                     </div>
                     <div class="last-reply">
-                        March 2024 <br> By <b><a href="#">User</a></b>
+                        March 2024 <br> By <b><a href="#">Sara</a></b>
                     </div>
                 </div>
 
                 <div class="table-row">
-                    <div class="status"><i class="fa fa-fire"></i></div>
-                    <div class="subjects"><a href="#">Is learning Python worth it in 2024?</a><br><span>Started by <b><a
-                                    href="#">User</a></b>.</span></div>
+                    <div class="status"><i class="fa fa-dragon"></i></div>
+                    <div class="subjects"><a href="#">Is learning SQL worth it in 2024?</a><br><span>Started by <b><a
+                                    href="#">Android</a></b>.</span></div>
                     <div class="replies">
                         2 replies <br> 125 views
                     </div>
                     <div class="last-reply">
-                        March 2024 <br> By <b><a href="#">User</a></b>
+                        March 2024 <br> By <b><a href="#">Android</a></b>
                     </div>
                 </div>
             </div>
@@ -86,6 +86,22 @@ export default {
             </div>
         </div>
 
+        <!-- status note -->
+
+        <div class="note">
+            <span>
+                <i class="fa fa-frown"></i>&nbsp; 0 Engagement Topic
+            </span><br>
+            <span>
+                <i class="fa fa-book"></i>&nbsp; Low Engagement Topic
+            </span><br>
+            <span>
+                <i class="fa fa-fire"></i>&nbsp; Popular Topic
+            </span><br>
+            <span>
+                <i class="fa fa-lock"></i>&nbsp;Closed Engagement Topic
+            </span><br>
+        </div>
 
     </main>
 </template>
@@ -176,6 +192,37 @@ main {
 
     & a:hover {
         opacity: .5;
+    }
+}
+
+/* navigation */
+
+.navigate {
+    margin-block: 20px;
+    font-weight: lighter;
+    font-size: 24px;
+
+    & a {
+        color: #fff;
+    }
+
+    & a:hover {
+        color: #ff0042;
+        font-weight: bolder;
+    }
+
+}
+
+/* status note */
+
+.note {
+    background-color: #111314;
+    padding: 20px;
+    display: block;
+
+    & span {
+        font-size: 20px;
+        margin-block: .7rem;
     }
 }
 </style>
