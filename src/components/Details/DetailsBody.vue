b1f2bb9d0d3e92f2fae3b53f5e4d52a400efce57
simonemoretti94 <simone.moretti.944@gmail.com>
2024-04-02 17:36:23 +0200

    on details body

--- src/components/Details/DetailsBody.vue
+++ src/components/Details/DetailsBody.vue
@@ -6,6 +6,60 @@ export default {
 
 <template>
     <div class="container">
+        <div class="posts-table">
+            <div class="navigate">
+                <span><a href="#">My Forum - Forums</a> >> <a href="#">Random Forum</a></span> >> <span><a
+                        href="#">Random Post</a></span>
+            </div>
+        </div>
+    </div>
 
+    <div class="topic-container">
+        <div class="head">
+            <div class="author">Author</div>
+            <div class="content">Topic: post's title (Read 1258 Times)</div>
+        </div>
+
+        <div class="body">
+            <div class="authors"></div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+/* navigation */
+.container {
+    & .navigate {
+        margin-block: 20px;
+        font-weight: lighter;
+        font-size: 24px;
+
+        & a {
+            color: #fff;
+        }
+
+        & a:hover {
+            color: #ff0042;
+            font-weight: bolder;
+        }
+    }
+
+
+    & .head {
+        display: flex;
+        background-color: #2c2c2c;
+        padding: 5px;
+        font-weight: bold;
+        font-size: 15px;
+
+        & .authors {
+            flex: 20%;
+        }
+
+        & .content {
+            flex: 80%;
+        }
+    }
+
+}
+</style>
